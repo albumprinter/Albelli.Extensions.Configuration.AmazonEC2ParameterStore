73e43ad63d66b6c5b9bbc56b9900429b2d62d7da
Anatolii Klots <A-KL@users.noreply.github.com>
2017-12-28 15:08:47 +0100

    Store lambda settings in EC2 ParameterStore (#50)

---
+++ AmazonEC2ParameterStoreSource.cs
@@ -0,0 +1,54 @@
+﻿namespace Albelli.AmazonEC2ParameterStore
+{
+    using System;
+
+    using Amazon;
+    using Amazon.Runtime;
+    using Amazon.SimpleSystemsManagement;
+
+    using JetBrains.Annotations;
+
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
+
+    internal sealed class AmazonEC2ParameterStoreSource : IConfigurationSource
+    {
+        public AmazonEC2ParameterStoreSource([NotNull] ILoggerFactory loggerFactory, [NotNull] string rootPath, [NotNull] string regionName)
+            : this(loggerFactory, rootPath, RegionEndpoint.GetBySystemName(regionName))
+        {  }
+
+        public AmazonEC2ParameterStoreSource([NotNull] ILoggerFactory loggerFactory, [NotNull] string rootPath, [NotNull] RegionEndpoint region)
+        {
+            this.LoggerFactory = loggerFactory ?? throw new ArgumentNullException(nameof(loggerFactory));
+
+            this.RootPath = rootPath ?? throw new ArgumentNullException(nameof(loggerFactory));
+
+            this.AmazonSimpleSystemsManagement = new AmazonSimpleSystemsManagementClient(region);
+        }
+
+        /// <summary>
+        /// A root directory to load settings from.
+        /// </summary>
+        public string RootPath { get; }
+
+        /// <summary>
+        /// Logger factory to be passed to the provider.
+        /// </summary>
+        private ILoggerFactory LoggerFactory { get; }
+
+        /// <summary>
+        /// AmazonSimpleSystemsManagement service to be passed to the provider.
+        /// </summary>
+        private IAmazonSimpleSystemsManagement AmazonSimpleSystemsManagement { get; }
+
+        /// <summary>
+        /// Builds the <see cref="AmazonEC2ParameterStoreProvider"/> for this source.
+        /// </summary>
+        /// <param name="builder">The <see cref="IConfigurationBuilder"/>The configuration builder.</param>
+        /// <returns>A <see cref="AmazonEC2ParameterStoreProvider"/>The EC2 ParameterStore provider.</returns>
+        public IConfigurationProvider Build(IConfigurationBuilder builder)
+        {
+            return new AmazonEC2ParameterStoreProvider(this.LoggerFactory, this.AmazonSimpleSystemsManagement, this.RootPath);
+        }
+    }
+}
