37aef98c3d91a2ee05c17805c5e1a2ac522a7d4a
Denys Zhuravel <derwasp@users.noreply.github.com>
2018-01-08 14:08:29 +0100

    Initial provider commit (#1)

---
+++ src/Albelli.Extensions.Configuration.AmazonEC2ParameterStore/AmazonEC2ParameterStoreProvider.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Amazon.SimpleSystemsManagement;
+using Amazon.SimpleSystemsManagement.Model;
+using JetBrains.Annotations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Albelli.Extensions.Configuration.AmazonEC2ParameterStore
+{
+    public sealed class AmazonEC2ParameterStoreProvider : ConfigurationProvider
+    {
+        private readonly string rootPath;
+        private readonly IAmazonSimpleSystemsManagement client;
+        private readonly ILogger<AmazonEC2ParameterStoreProvider> logger;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="AmazonEC2ParameterStoreProvider"/>.
+        /// </summary>
+        public AmazonEC2ParameterStoreProvider([NotNull] ILoggerFactory loggerFactory, [NotNull] IAmazonSimpleSystemsManagement client, [NotNull] string rootPath)
+        {
+            this.rootPath = rootPath ?? throw new ArgumentNullException(nameof(rootPath));
+            this.client = client ?? throw new ArgumentNullException(nameof(client));
+
+            this.logger = loggerFactory?.CreateLogger<AmazonEC2ParameterStoreProvider>() ?? throw new ArgumentNullException(nameof(loggerFactory));
+        }
+
+        public override void Load()
+        {
+            try
+            {
+                this.LoadAsync().GetAwaiter().GetResult();
+            }
+            catch (Exception e)
+            {
+                this.logger.LogError(0, e, "Failed to access Amazon EC2 ParameterStore");
+            }
+        }
+
+        private async Task LoadAsync()
+        {
+            foreach (var pair in await this.GetParametersAsync(this.client, this.rootPath))
+            {
+                this.Data.Add(pair.Key, pair.Value);
+            }
+        }
+
+        private async Task<IDictionary<string, string>> GetParametersAsync(IAmazonSimpleSystemsManagement ssm, string path)
+        {
+            var request = new GetParametersByPathRequest
+            {
+                Path = path,
+                MaxResults = 10,
+                WithDecryption = true,
+                Recursive = true
+            };
+
+            var result = new Dictionary<string, string>();
+
+            do
+            {
+                var response = await ssm.GetParametersByPathAsync(request);
+
+                this.logger.LogInformation("EC2 ParameterStore has {ParametersCount} parameters in \'{ParametersPath}\'",
+                    response.Parameters.Count,
+                    path);
+
+                foreach (var resultParameter in response.Parameters)
+                {
+                    if (resultParameter.Type == ParameterType.SecureString)
+                    {
+                        this.logger.LogInformation("EC2 ParameterStore has returned {ParameterName}", resultParameter.Name);
+                    }
+                    else
+                    {
+                        this.logger.LogInformation("EC2 ParameterStore has returned {ParameterName}  with value \'{ParameterValue}\'", resultParameter.Name, resultParameter.Value);
+                    }
+
+                    var convertedKey = resultParameter.Name
+                        .Trim('/')
+                        .Replace("/", ":");
+
+                    result.Add(convertedKey, resultParameter.Value);
+                }
+
+                request.NextToken = response.NextToken;
+
+            } while (!string.IsNullOrEmpty(request.NextToken));
+
+            this.logger.LogInformation(
+                "EC2 ParameterStore has returned {TotalParametersCount} parameters in total",
+                result.Count);
+
+            return result;
+        }
+    }
+}
---
+++ src/Albelli.Extensions.Configuration.AmazonEC2ParameterStore/AmazonEC2ParameterStoreProviderExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Amazon;
+using JetBrains.Annotations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Albelli.Extensions.Configuration.AmazonEC2ParameterStore
+{
+    public static class AmazonEC2ParameterStoreProviderExtensions
+    {
+        /// <summary>
+        /// Adds an <see cref="IConfigurationProvider"/> 
+        /// that reads configuration values from AWS EC2 ParameterStore and decrypt them using AWS KMS.
+        /// </summary>
+        /// <param name="configurationBuilder">The <see cref="IConfigurationBuilder"/> to add to.</param>
+        /// <param name="loggerFactory">Logger factory to be passed to the provider.</param>
+        /// <param name="rootPath">Parameters directory to load from.</param>
+        /// <param name="region">Parameters AWS region.</param>
+        /// <returns>The <see cref="AmazonEC2ParameterStoreSource"/>.</returns>
+        public static IConfigurationBuilder AddEC2ParameterStoreVariables(
+            [NotNull] this IConfigurationBuilder configurationBuilder,
+            [NotNull] ILoggerFactory loggerFactory,
+            [NotNull] string rootPath,
+            [NotNull] string region)
+        {
+            if (loggerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(loggerFactory));
+            }
+
+            return configurationBuilder.Add(new AmazonEC2ParameterStoreSource(loggerFactory, rootPath, region));
+        }
+
+        /// <summary>
+        /// Adds an <see cref="IConfigurationProvider"/> 
+        /// that reads configuration values from AWS EC2 ParameterStore and decrypt them using AWS KMS.
+        /// </summary>
+        /// <param name="configurationBuilder">The <see cref="IConfigurationBuilder"/> to add to.</param>
+        /// <param name="loggerFactory">Logger factory to be passed to the provider.</param>
+        /// <param name="rootPath">Parameters directory to load from.</param>
+        /// <param name="region">Parameters AWS region.</param>
+        /// <returns>The <see cref="AmazonEC2ParameterStoreSource"/>.</returns>
+        public static IConfigurationBuilder AddEC2ParameterStoreVariables(
+            [NotNull] this IConfigurationBuilder configurationBuilder,
+            [NotNull] ILoggerFactory loggerFactory,
+            [NotNull] string rootPath,
+            [NotNull] RegionEndpoint region)
+        {
+            if (loggerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(loggerFactory));
+            }
+
+            return configurationBuilder.Add(new AmazonEC2ParameterStoreSource(loggerFactory, rootPath, region));
+        }
+    }
+}
